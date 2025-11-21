d23a5e5813480b40c72d66baa100e94cbb4cae43
andywijaya15 <andykevonata@gmail.com>
2025-11-21 16:26:57 +0700

    add analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Home } from "./components/sections/Home";
+import { Analytics } from "@vercel/analytics/next";
 
 export const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
       <Home />
+      <Analytics />
     </>
   );
 };
